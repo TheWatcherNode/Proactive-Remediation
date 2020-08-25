9e3090531169b0414df904d5e2103d10b12b7cf7
The Watcher <23211597+TheWatcherNode@users.noreply.github.com>
2020-08-25 18:13:23 +0100

    Version 1.0

---
+++ Detect_Update_PowerShell_Modules.ps1
@@ -0,0 +1,31 @@
+﻿#=============================================================================================================================
+#
+# Script Name:     Detect_Update_PowerShell_Modules.ps1
+# Description:     Detect outdated PowerShell Module and update from PowerShell Gallery
+# Notes:           The purpose of this script is to keep installed PowerShell Modules on IT Administrator Devices up to 
+#                  date with the latest PowerShell Modules from the PowerShell Gallery
+#
+#=============================================================================================================================
+
+# Define Variables
+$Array = @(Get-InstalledModule)
+$Green = 'Green'
+$DarkRed = 'DarkRed'
+$DarkCyan = 'DarkCyan'
+$White = 'White'
+
+   Foreach ($Module in $Array)
+   
+   {
+     $ModuleCheck = Get-InstalledModule -name $Module.Name -ErrorAction SilentlyContinue
+     $online = Find-Module -Name $module.name -Repository PSGallery -ErrorAction Stop   
+
+     if ($ModuleCheck.version -EQ $online.Version) {
+           Write-Host "Detected: "$Module.Name, "doesnt require an update as installed version" $Module.Version,"matches the latest online version" $online.Version -ForegroundColor $Green
+          
+          }
+     else {
+           Write-Host "Detected:"$Module.Name "Module", "is running an outdate version" $Module.Version, "Version"$online.Version, "is now available" -ForegroundColor $DarkRed
+           exit 0
+          }
+          }
---
+++ Remediate_Update_PowerShell_Modules.ps1
@@ -0,0 +1,34 @@
+﻿#=============================================================================================================================
+#
+# Script Name:     Remediate_Update_PowerShell_Modules.ps1
+# Description:     Remediate outdated PowerShell Module and update from PowerShell Gallery
+# Notes:           The purpose of this script is to keep installed PowerShell Modules on IT Administrator Devices up to 
+#                  date with the latest PowerShell Modules from the PowerShell Gallery
+#
+#=============================================================================================================================
+
+# Define Variables
+$Array = @(Get-InstalledModule)
+$Green = 'Green'
+$DarkCyan = 'DarkCyan'
+$DarkRed = 'DarkRed'
+$White = 'White'
+
+
+   Foreach ($Module in $Array)
+   
+   {
+     $ModuleCheck = Get-InstalledModule -name $Module.Name -ErrorAction SilentlyContinue
+     $online = Find-Module -Name $module.name -Repository PSGallery -ErrorAction Stop   
+
+     if ($ModuleCheck.version -EQ $online.Version) {
+           Write-Host "Detected: "$Module.Name, "doesnt require an update as installed version" $Module.Version,"matches the latest online version" $online.Version -ForegroundColor $Green 
+          }
+     else {
+           Write-Host "Detected:"$Module.Name "Module", "is running an outdate version" $Module.Version, "Version"$online.Version, "is now available" -ForegroundColor Red
+           Uninstall-Module -Name $Module.Name -RequiredVersion $module.version 
+           Write-Host -BackgroundColor DarkRed -ForegroundColor $White 'Info: Legacy Version of',$Module.name,'module now removed'
+           Install-Module -Name $Module.Name -RequiredVersion $online.Version -Force -allowclobber
+           exit 0
+          }
+          }
